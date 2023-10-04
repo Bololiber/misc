11b3cf012d5e657f331bbb5a4894007d128d5bad
Boliber <46798867+MagicKin45@users.noreply.github.com>
2023-10-04 13:00:21 -0400

    Update length.rs
    Here's a thing to make sure I'm not doing anything terribly wrong.

--- unit_convert/src/units/length.rs
+++ unit_convert/src/units/length.rs
@@ -36,6 +36,46 @@ impl_units! {
         League => [
             <| |l| l * 4828.0417,
             |> |m| m / 4828.0417
+        ],
+        Light-Year => [
+            <| |ly| ly * 9460730472580800,
+            |> |m| m / 9460730472580800,
+            aliases = ["ly"]
+        ],
+        Astronomical Unit => [
+            <| |au| au * 1.495978707e11,
+            |> |m| m / 1.495978707e11,
+            aliases = ["au"]
+        ],
+        Siriometer => [
+            <| |sir| sir * 1.495978707e17,
+            |> |m| m / 1.495978707e17,
+            aliases = ["sir"]
+        ],
+        Parsec => [
+            <| |pc| pc * 3.0856776e16,
+            |> |m| m / 3.0856776e16,
+            aliases = ["pc"]
+        ],
+        Potrzebie => [
+            <| |p| p * 0.0022633484517438173216473,
+            |> |m| m / 0.0022633484517438173216473,
+            aliases = ["p"]
+        ],
+        Farshimmelt Potrzebie => [
+            <| |fp| fp * 0.0022633484517438173216473e-6,
+            |> |m| m / 0.0022633484517438173216473e-6,
+            aliases = ["fp"]
+        ],
+        Furshlugginer Potrezebie => [
+            <| |fup| fup * 0.0022633484517438173216473e6,
+            |> |m| m / 0.0022633484517438173216473e6,
+            aliases = ["fup"]
+        ],
+        Furlong => [
+            <| |fur| fur * 201.168,
+            |> |m| m / 201.168,
+            aliases = ["fur"]
         ]
     }
 }
